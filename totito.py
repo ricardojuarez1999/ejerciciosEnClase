a546cadd49cb9c8f5a15c5eb9c9e9c535d4b7562
ricardojuarez1999 <ricardojuarez1999@gmail.com>
2016-07-13 23:18:19 -0600

    termine el totito

--- totito.py
+++ totito.py
@@ -1,41 +1,61 @@
-a = 10
+ganador = False
 tablero = [[65,66,67],[68,69,70],[71,72,73]]
 fichas = [[0,0,0],[0,0,0],[0,0,0]]
 letras = []
-while a !=20:
+print (chr(tablero[0][0]), chr(tablero[0][1]), chr(tablero[0][2]))
+print (chr(tablero[1][0]), chr(tablero[1][1]), chr(tablero[1][2]))
+print (chr(tablero[2][0]), chr(tablero[2][1]), chr(tablero[2][2]))
+while ganador == False :
+	seriedad = False
 	sumas = []
-	s2 = 0
-	s4 = 0
-	s5 = 0
+	while seriedad != True:
+		x0 = ord(input("jugador 1, ingrese X: "))
+		if x0 <= 64 or x0 >= 74:
+			print ("jugador 1, ese caracter no es valido, sea serio por favor.")
+		else:
+			x = x0 - 65
+			fila = x % 3
+			columna  = x // 3
+			if x0 in letras:
+				print ("esa casilla esta siendo usada, jugardor 1 sea serio, por favor hagalo de nuevo.")
+			else:
+				letras.append(x0)
+				tablero[columna][fila] = 88
+				fichas[columna][fila] = 1
+				seriedad = True
 	print (chr(tablero[0][0]), chr(tablero[0][1]), chr(tablero[0][2]))
 	print (chr(tablero[1][0]), chr(tablero[1][1]), chr(tablero[1][2]))
 	print (chr(tablero[2][0]), chr(tablero[2][1]), chr(tablero[2][2]))
-	x0 = ord(input("jugador 1, ingrese X: "))
-	x = x0 - 65
-	fila = x % 3
-	columna  = x //3
-	if x0 in letras:
-		print ("esa casilla esta siendo usada, deseperdicio un turno.")
-	else:
-		letras.append(x0)
-		tablero[columna][fila] = 88
-		fichas[columna][fila] = 1
-	print (chr(tablero[0][0]), chr(tablero[0][1]), chr(tablero[0][2]))
-	print (chr(tablero[1][0]), chr(tablero[1][1]), chr(tablero[1][2]))
-	print (chr(tablero[2][0]), chr(tablero[2][1]), chr(tablero[2][2]))
-	o0 = ord(input("jugador 2, ingrese O: "))
-	o = o0 - 65
-	fila = o % 3
-	columna  = o //3
-	if o0 in letras:
-		print ("esa casilla esta siendo usada, desperdicio un turno.")
-	else:
-		letras.append(o0)
-		tablero[columna][fila] = 79
-		fichas[columna][fila] = -1
+	while seriedad != False:
+		o0 = ord(input("jugador 2, ingrese O: "))
+		if o0 <= 64 or o0 >= 74:
+			print ("jugador 2, ese caracter no es valido, sea serio por favor.")
+		else:
+			o = o0 - 65
+			fila = o % 3
+			columna  = o //3
+			if o0 in letras:
+				print ("esa casilla esta siendo usada, jugardor 2 sea serio, por favor hagalo de nuevo.")
+			else:
+				letras.append(o0)
+				tablero[columna][fila] = 79
+				fichas[columna][fila] = -1
+				seriedad = False
 	for i in range(3):
-		s = sum(fichas[i])
-		sumas.append(s)
-		s1 = (fichas[0][i] + fichas[1][i] + fichas[2][i])
-		sumas.append(s1)
-	print (sumas)
+		horizontal = sum(fichas[i])
+		sumas.append(horizontal)
+		vertical = (fichas[0][i] + fichas[1][i] + fichas[2][i])
+		sumas.append(vertical)
+	diagonal = (fichas[0][0]+fichas[1][1]+fichas[2][2])
+	diagonal2 = (fichas[0][2]+fichas[1][1]+fichas[2][0])
+	sumas.append(diagonal)
+	sumas.append(diagonal2)
+	print (chr(tablero[0][0]), chr(tablero[0][1]), chr(tablero[0][2]))
+	print (chr(tablero[1][0]), chr(tablero[1][1]), chr(tablero[1][2]))
+	print (chr(tablero[2][0]), chr(tablero[2][1]), chr(tablero[2][2]))
+	if 3 in sumas:
+		print ("el jugador 1 ha ganado")
+		ganador = True
+	elif -3 in sumas:
+		print ("el ganador 2 ha ganado")
+		ganador = True
