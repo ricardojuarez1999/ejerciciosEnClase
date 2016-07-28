1ac75e4c543d6763178edf774c7c3ca84a7ebf3a
ricardojuarez1999 <ricardojuarez1999@gmail.com>
2016-07-28 14:26:39 -0600

    termine

--- prueba.py
+++ prueba.py
@@ -1,9 +1,11 @@
 import ahorcado
-palabra = list(ahorcado.palabra_r())
+palabra = (ahorcado.palabra_r())
 turnos = 0
 guiones = []
+ganar = 0
 for i in range(len(palabra)):
 	guiones += "_"
+print ("Bienvenido a ahorcado: \n \n     las reglas son simples \n     si usted falla 7 veces perdera \n     que comience el juego... \n")
 while turnos != 7:
 	gui = ""
 	letras2 = 0
@@ -14,8 +16,13 @@ while turnos != 7:
 	for i in range(len(guiones)):
 		if letra == palabra[i]:
 			guiones[i] = letra
+			ganar +=1
 		else:
 			letras2 += 1
 	if letras2 == len(guiones):
 		turnos += 1
 		print (ahorcado.suicida(turnos-1))
+	if ganar == len(palabra):
+		print ("\nusted ha ganado")
+		turnos = 7
+print ("la palabra correcta era", palabra)
